3b14c65b6426a94e4d7bd5d9eff6c2b4fce0bd61
knubbbe <knubbbe@gmail.com>
2019-02-25 15:55:59 +0100

    first commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,34 @@
+<div class="counter" on:click="set({ count: count + 1 })">
+    <div class="counter__number">
+        {count}
+    </div>
+</div>
+
+<style>
+.counter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 15rem;
+    height: 15rem;
+    background-color: #fff;
+    border: 1px solid #fcfcfc;
+    border-radius: 100%;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, .1);
+    cursor: pointer;
+}
+
+.counter__number {
+    font-size: 6rem;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    }
+}
+</script>
