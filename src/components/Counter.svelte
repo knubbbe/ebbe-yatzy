0aa4fa79aa2a08b42e373e5585d9ddecbab7b1d9
knubbbe <knubbbe@gmail.com>
2019-02-25 16:22:18 +0100

    some transitions on button

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,8 +1,8 @@
 <div class="counter">
-    <div class="counter__number" on:click="set({ count: count + 1 })">
+    <button class="counter__number" on:click="set({ count: count + 1 })">
         <span>{count}</span>
-    </div>
-    <button on:click="set({ count: 0 })">reset</button>
+    </button>
+    <button class="counter__reset" on:click="set({ count: 0 })">reset</button>
 </div>
 
 <style>
@@ -21,16 +21,26 @@
     height: 15rem;
     font-size: 6rem;
     background-color: #fff;
-    border: 1px solid #fcfcfc;
+    border: 1px solid #eee;
     border-radius: 100%;
-    box-shadow: 2px 2px 10px rgba(0, 0, 0, .1);
+    box-shadow: 2px 2px 15px 0 rgba(0, 0, 0, .1);
     cursor: pointer;
-}
-.counter__number span {
-    line-height: 1;
+    transition: all .1s ease-in;
 }
 
-button {
+.counter__number span {
+    line-height: 1;
+    transition: all .1s ease-in;
+}
+
+.counter__number:active {
+    box-shadow: 0px 0px 3px rgba(0, 0, 0, .1);
+}
+.counter__number:active span {
+    transform: scale(0.8);
+}
+
+.counter__reset {
     margin-top: 2rem;
 }
 </style>
