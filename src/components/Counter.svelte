b1f76d2690ade8ccb37d8496d27caacacc3548b7
knubbbe <knubbbe@gmail.com>
2019-02-25 16:06:17 +0100

    added reset button

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,25 +1,37 @@
-<div class="counter" on:click="set({ count: count + 1 })">
-    <div class="counter__number">
-        {count}
+<div class="counter">
+    <div class="counter__number" on:click="set({ count: count + 1 })">
+        <span>{count}</span>
     </div>
+    <button on:click="set({ count: 0 })">reset</button>
 </div>
 
 <style>
 .counter {
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.counter__number {
+    display: flex;
     justify-content: center;
     align-items: center;
     width: 15rem;
     height: 15rem;
+    font-size: 6rem;
     background-color: #fff;
     border: 1px solid #fcfcfc;
     border-radius: 100%;
     box-shadow: 2px 2px 10px rgba(0, 0, 0, .1);
     cursor: pointer;
 }
+.counter__number span {
+    line-height: 1;
+}
 
-.counter__number {
-    font-size: 6rem;
+button {
+    margin-top: 2rem;
 }
 </style>
 
